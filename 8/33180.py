851e40aad84441b0284acc9b501fe8cc08b91b2a
Андрей Боев <a.boev.it@gmail.com>
2023-12-21 17:19:05 +0300

    Задание 12

--- 8/33180.py
+++ 8/33180.py
@@ -1,11 +1,11 @@
 import itertools
 
 count = 0
-listString = itertools.product('ТИМОФЕЙ',repeat= 5)
+listString = itertools.product('ТИМОФЕЙ', repeat=5)
 
 for str in listString:
     line = ''.join(str)
     if line[0] != 'Й' and line[4] != 'Й':
-        if line.count('Й') < 2 and line.count('ЙИ') == 0 and line.count('ИЙ') ==0:
+        if line.count('Й') < 2 and line.count('ЙИ') == 0 and line.count('ИЙ') == 0:
             count += 1
 print(count)
